ac4000fd2b5ef6d48309ca261dc517355a75e9e3
Mat Sletten <matsletten@mmintzmbp.bbbb.net>
2014-10-21 16:50:54 -0700

    Initial Commit

---
+++ Table Views/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Table Views
+//
+//  Created by Mat Sletten on 10/21/14.
+//  Copyright (c) 2014 Mat Sletten. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
