0dc7d270cd0d11e8da012189543905c0ad02fe2b
Mat Sletten <matsletten@reception-desk.bbbb.net>
2015-01-30 11:27:40 -0800

    updated files

--- Table Views/ViewController.swift
+++ Table Views/ViewController.swift
@@ -8,7 +8,24 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    let array = ["Mat", "Jennifer", "Melissa", "Chuck"]
+    
+    func tableView(tableView: UITableView!, numberOfRowsInSection section: Int) -> Int
+    {
+        return array.count
+    }
+    
+    // Row display. Implementers should *always* try to reuse cells by setting each cell's reuseIdentifier and querying for available reusable cells with dequeueReusableCellWithIdentifier:
+    // Cell gets various attributes set automatically based on table (separators) and data source (accessory views, editing controls)
+    
+    func tableView(tableView: UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell!
+    {
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell")
+        cell.textLabel?.text = array[indexPath.row]
+        return cell
+    }
                             
     override func viewDidLoad() {
         super.viewDidLoad()
